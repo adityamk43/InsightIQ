910d17c1596d77898090f7fb03193b19d79d8f66
adityamk43 <adityamk43@gmail.com>
2023-03-03 20:01:30 +0530

    Minor Changes

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -8,7 +8,7 @@ const NotFound = () => {
             <div className="d-flex align-items-center justify-content-center vh-100">
                 <div className="text-center">
                     <h1 className="display-1 fw-bold">404</h1>
-                    <p className="fs-3"> <span class="text-danger">Opps!</span> Page not found.</p>
+                    <p className="fs-3"> <span className="text-danger">Opps!</span> Page not found.</p>
                     <p className="lead">
                         The page you’re looking for doesn’t exist.
                     </p>
