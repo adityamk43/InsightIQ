3ad7b796edd875cda3e359ba54430bf3ab8762f6
adityamk43 <adityamk43@gmail.com>
2023-03-03 20:00:56 +0530

    Minor Changes

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -5,11 +5,11 @@ import './NotFound.css';
 const NotFound = () => {
     return (
         <div className="NotFound">
-            <div class="d-flex align-items-center justify-content-center vh-100">
-                <div class="text-center">
-                    <h1 class="display-1 fw-bold">404</h1>
-                    <p class="fs-3"> <span class="text-danger">Opps!</span> Page not found.</p>
-                    <p class="lead">
+            <div className="d-flex align-items-center justify-content-center vh-100">
+                <div className="text-center">
+                    <h1 className="display-1 fw-bold">404</h1>
+                    <p className="fs-3"> <span class="text-danger">Opps!</span> Page not found.</p>
+                    <p className="lead">
                         The page you’re looking for doesn’t exist.
                     </p>
                     <Link to="/" className="btn GoHome fs-4">Go Home</Link>
